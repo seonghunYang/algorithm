9d1ca77530d812a76b9a1d6ff4b4404e8dd7886b
seonghunYang <didtjdgns852@naver.com>
2021-01-28 00:53:17 +0900

    14500

--- test.py
+++ test.py
@@ -1,37 +1,59 @@
-#14503
-# 북, 동, 남, 서
-dx = [0, -1, 0, 1]
-dy = [-1, 0, 1, 0]
+#14500 테르토미노
+# 교훈.. 단순하게 처리하려는 것 보다 단순 노가다가 좋을 떄가 있다
+import copy
+def process(shape):
+    max_val = 0
+    for i in range(N):
+        for j in range(M):
+            try: 
+                tmp = data[i][j]
+                for n, m in shape:
+                    x, y = i + n, j + m
+                    tmp += data[x][y]
+                max_val = max(max_val, tmp)
+            except IndexError:
+                continue
+    return max_val
 
-def check(n, m):
-    return 0 <= n <= N-1 and 0 <= m <= M-1
+def rotate(lst, n, m):
+    new_lst = [[0] * m for _ in range(n)]
+    for i in range(m):
+        for j in range(n):
+            new_lst[j][m - i - 1] = lst[i][j]
+    return new_lst
+
+def mirror1(lst):
+    new_lst = copy.deepcopy(lst)
+    for i in range(N):
+        for j in range(M):
+            new_lst[i][M-j-1] = lst[i][j]
+    return new_lst
+
+def mirror2(lst):
+    new_lst = copy.deepcopy(lst)
+    for i in range(N):
+        for j in range(M):
+            new_lst[N-i-1][j] = lst[i][j]
+    return new_lst
+
+a = [(0, 1), (0, 2), (0, 3)]
+b = [(0, 1), (1, 0), (1, 1)]
+c = [(1, 0), (2, 0), (2, 1)]
+d = [(1, 0), (1, 1), (2, 1)]
+e = [(0, 1), (0, 2), (1, 1)]
 
 N, M = map(int, input().split())
-r, c, d = map(int, input().split())
 data = [list(map(int, input().split())) for _ in range(N)]
-visited = [[0] * M for _ in range(N)]
 ans = 0
-while True:
-    if visited[r][c] == 0:
-        visited[r][c] = 1
-        ans += 1
-    isfind = False
+for el in [a, b, c, d, e]:
     for _ in range(4):
-        #n , m은 탐색할 좌표 
-        n, m = r + dx[d], c + dy[d]
-        if  check(n,m) and data[n][m] != 1 and visited[n][m] == 0:
-            d = (d - 1) % 4
-            r, c = n, m
-            isfind = True
-            break
-        else:
-            d = (d - 1) % 4
-    if not isfind:
-        # 후진 좌표
-        n = r + dx[d-1]
-        m = c + dy[d-1]
-        if not check(n,m) or data[n][m] == 1 :
-            break
-        else:
-            r, c = n, m
+        ans = max(ans, process(el))
+        data = mirror1(data)
+        ans = max(ans, process(el))
+        data = mirror1(data)
+        data = mirror2(data)
+        ans = max(ans, process(el))
+        data = mirror2(data)
+        N, M = M, N
+        data = rotate(data, N, M)
 print(ans)
