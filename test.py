aacf08cb295f06219293d04d049017a7bb204438
seonghunYang <didtjdgns852@naver.com>
2021-01-29 21:19:11 +0900

    3190

--- test.py
+++ test.py
@@ -1,59 +1,45 @@
-#14500 테르토미노
-# 교훈.. 단순하게 처리하려는 것 보다 단순 노가다가 좋을 떄가 있다
-import copy
-def process(shape):
-    max_val = 0
-    for i in range(N):
-        for j in range(M):
-            try: 
-                tmp = data[i][j]
-                for n, m in shape:
-                    x, y = i + n, j + m
-                    tmp += data[x][y]
-                max_val = max(max_val, tmp)
-            except IndexError:
-                continue
-    return max_val
+#3190
 
-def rotate(lst, n, m):
-    new_lst = [[0] * m for _ in range(n)]
-    for i in range(m):
-        for j in range(n):
-            new_lst[j][m - i - 1] = lst[i][j]
-    return new_lst
+# D => idx 증가 L => idx 감소
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 
-def mirror1(lst):
-    new_lst = copy.deepcopy(lst)
-    for i in range(N):
-        for j in range(M):
-            new_lst[i][M-j-1] = lst[i][j]
-    return new_lst
+N = int(input())
+K = int(input())
+data = [[0] * N for _ in range(N)]
+for _ in range(K):
+    r, c = map(int, input().split())
+    data[r-1][c-1] = 1
+L = int(input())
+L_dic = {}
+for _ in range(L):
+    x, C = input().split()
+    L_dic[int(x)] = C
 
-def mirror2(lst):
-    new_lst = copy.deepcopy(lst)
-    for i in range(N):
-        for j in range(M):
-            new_lst[N-i-1][j] = lst[i][j]
-    return new_lst
-
-a = [(0, 1), (0, 2), (0, 3)]
-b = [(0, 1), (1, 0), (1, 1)]
-c = [(1, 0), (2, 0), (2, 1)]
-d = [(1, 0), (1, 1), (2, 1)]
-e = [(0, 1), (0, 2), (1, 1)]
-
-N, M = map(int, input().split())
-data = [list(map(int, input().split())) for _ in range(N)]
-ans = 0
-for el in [a, b, c, d, e]:
-    for _ in range(4):
-        ans = max(ans, process(el))
-        data = mirror1(data)
-        ans = max(ans, process(el))
-        data = mirror1(data)
-        data = mirror2(data)
-        ans = max(ans, process(el))
-        data = mirror2(data)
-        N, M = M, N
-        data = rotate(data, N, M)
-print(ans)
+    
+visited = [[-1] * N for _ in range(N)]
+sec = 0
+visited[0][0] = 0
+head = [0, 0]
+tail = [0, 0]
+vec = 0
+while True:
+    sec += 1
+    head = [head[0] + dx[vec], head[1] + dy[vec]]
+    if  head[0] < 0 or head[0] > N-1 or head[1] < 0 or head[1] > N-1 or visited[head[0]][head[1]] >= visited[tail[0]][tail[1]] :
+        break
+    visited[head[0]][head[1]] = sec
+    if data[head[0]][head[1]] == 1:
+        data[head[0]][head[1]] = 0
+    else:
+        for i in range(4):
+            x, y = tail[0] + dx[i], tail[1] + dy[i]
+            if 0 <= x <= N-1 and 0 <= y <= N-1 and visited[x][y] == visited[tail[0]][tail[1]] + 1:
+                tail = [x, y]
+                break
+    if L_dic.get(sec):
+        if L_dic[sec] == "D":
+            vec = (vec + 1) % 4
+        else:
+            vec = (vec - 1) % 4
+print(sec)
